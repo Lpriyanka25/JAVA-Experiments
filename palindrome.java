f94267e1f8e503c97a3e600d4e2a8330e2dceb53
Priyanka Langote <161610464+Lpriyanka25@users.noreply.github.com>
2024-11-24 23:28:17 +0530

    Update palindrome.java

--- palindrome.java
+++ palindrome.java
@@ -1,3 +1,4 @@
+//example 1
 public class PalindromeNumber {
 
     public static void main(String[] args) {
@@ -17,5 +18,33 @@ public class PalindromeNumber {
         
         // Check if the string is equal to its reverse
         return str.equals(new StringBuilder(str).reverse().toString());
+    }
+}
+
+// example 2
+public class PalindromeNumber {
+
+    public static void main(String[] args) {
+        int number = 12321; // Example number to check
+        boolean isPalindrome = isPalindromeNumber(number);
+
+        if (isPalindrome) {
+            System.out.println(number + " is a palindrome number.");
+        } else {
+            System.out.println(number + " is not a palindrome number.");
+        }
+    }
+
+    public static boolean isPalindromeNumber(int number) {
+        int originalNumber = number;
+        int reversedNumber = 0;
+
+        while (number != 0) {
+            int digit = number % 10;
+            reversedNumber = reversedNumber * 10 + digit;
+            number /= 10;
+        }
+
+        return originalNumber == reversedNumber;
     }
 }
