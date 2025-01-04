97cc123dcf7bfede876ac1546325d5cf201e4227
Priyanka Langote <161610464+Lpriyanka25@users.noreply.github.com>
2025-01-04 23:27:17 +0530

    Create codechef.java

---
+++ codechef.java
@@ -0,0 +1,13 @@
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		int a = 0;
+        // Write the loop here
+        while (a < 7) {
+            System.out.println(a);
+            a++;
+        }
+        
+	}
+}
