ead3c06d64d8256e927a8912bf2663e20dd7404d
Priyanka Langote <161610464+Lpriyanka25@users.noreply.github.com>
2025-01-06 23:58:28 +0530

    Create arithmetic.java

---
+++ arithmetic.java
@@ -0,0 +1,9 @@
+class Codechef
+{
+    public static void main (String[] args)
+    {
+        // Solution as follows
+        System.out.println(9 - 2);
+        System.out.println(30 / 5);
+    }
+}
