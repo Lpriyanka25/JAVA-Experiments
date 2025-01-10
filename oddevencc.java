98d46bb27b068a6b24766fdd196d613ff7a325c6
Priyanka Langote <161610464+Lpriyanka25@users.noreply.github.com>
2025-01-10 22:24:46 +0530

    Create oddevencc.java

---
+++ oddevencc.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Codechef {
+    public static void main(String[] args) {
+        int n;
+        n = 5;
+        if (n % 2 == 1) {
+            System.out.println("Odd");
+        } else {
+            System.out.println("Even");
+        }     
+    }
+}
