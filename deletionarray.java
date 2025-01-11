37b0e61929e0a77b16160435871a90e02c82b015
Priyanka Langote <161610464+Lpriyanka25@users.noreply.github.com>
2025-01-11 19:55:16 +0530

    Create deletionarray.java

---
+++ deletionarray.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+		int[] array = new int[100]; // Initial array
+		array[0] = 2;
+	    array[1] = 4;
+	    array[2] = 6;
+	    array[3] = 8;
+	    array[4] = 10;
+        int size = 5; 
+
+        int positionToDelete = 2; 
+
+        for (int i = 0; i < size; i++) {
+            System.out.print(array[i] + " ");
+        }
+	}
+}
