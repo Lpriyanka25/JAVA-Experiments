6818bc18d3776e21afe85fd5b7e6479dcecad6ca
Priyanka Langote <161610464+Lpriyanka25@users.noreply.github.com>
2025-01-28 22:20:38 +0530

    Create operator_cc.java

---
+++ operator_cc.java
@@ -0,0 +1,10 @@
+class Codechef
+{
+	public static void main (String[] args)
+	{
+	    int length = 11;
+	   // Update your code below this line
+		length %= 3;
+       System.out.println(length);
+	}
+}
